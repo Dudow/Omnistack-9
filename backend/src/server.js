64eb5e0799a4d737ea59b1e44b16e97b2d335fd9
Eduardo <edu30.nm@gmail.com>
2020-07-14 17:58:32 -0300

    fim da segunda aula

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const routes = require('./routes')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://Omnistack:omnistack@omnistack9.qnwfq.mongodb.net/Omnistack9?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(routes)
