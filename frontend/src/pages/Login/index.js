f6fa26c20b8953d5544d9537d37e8aea12df6351
Eduardo <edu30.nm@gmail.com>
2020-07-17 15:41:43 -0300

    fim da terceira aula

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import api from '../../services/api'
+
+export default function Login({ history }) {
+
+    const [email, setEmail] = useState('')
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        const response = await api.post('/sessions', {
+            email,
+        })
+
+        const { _id } = response.data
+
+        localStorage.setItem('user', _id)
+
+        history.push('/dashboard')
+    }
+    return (
+        <>
+            <p>
+                Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
+            </p>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">
+                    E-mail
+            </label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Insira seu e-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+
+                <button type="submit" className="btn">
+                    Entrar
+                </button>
+
+            </form>
+        </>
+    )
+}
